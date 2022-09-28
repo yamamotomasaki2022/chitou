56e68d82977bdc851df4c8bd901d1fc1344e41b5
jacky <learningma0926@gmail.com>
2022-09-28 18:55:40 +0800

    220928-jacky-spring security start

--- src/main/java/tw/chitou/config/WebSecurityConfig.java
+++ src/main/java/tw/chitou/config/WebSecurityConfig.java
@@ -1,51 +1,51 @@
 package tw.chitou.config;
-//package tw.weber.config;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpMethod;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.builders.WebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//
-//import tw.leonchen.controller.AuthUserDetailsService;
-//
-//@SuppressWarnings("deprecation")
-////@EnableWebSecurity
-//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//	@Autowired
-//	private AuthUserDetailsService auds;
-//	
-//	@Override
-//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//		auth
-//			.userDetailsService(auds)
-//			.passwordEncoder(new BCryptPasswordEncoder());
-//			
-//	}
-//
-//	@Override
-//	public void configure(WebSecurity web) throws Exception {
-//	}
-//
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//		http.authorizeHttpRequests()
-//		.antMatchers(HttpMethod.GET,"/users/**").authenticated()
-//		.antMatchers(HttpMethod.GET).permitAll()
-//		.antMatchers(HttpMethod.POST,"/users/**").authenticated()
-//		.antMatchers(HttpMethod.POST).permitAll()
-//		.anyRequest().authenticated()
-//		.and()
-//		.rememberMe().tokenValiditySeconds(86400).key("rememberMe-Key")
-//		.and()
-//		.csrf().disable()
-//		.formLogin().loginPage("/login/page")
-//		//登入成功:導到原先瀏覽頁面，沒有就welcome
-//		.defaultSuccessUrl("/login/welcome");
-//	}
-//	
-//}
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import tw.leonchen.controller.AuthUserDetailsService;
+
+@SuppressWarnings("deprecation")
+//@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private AuthUserDetailsService auds;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.userDetailsService(auds)
+			.passwordEncoder(new BCryptPasswordEncoder());
+			
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests()
+		.antMatchers(HttpMethod.GET,"/users/**").authenticated()
+		.antMatchers(HttpMethod.GET).permitAll()
+		.antMatchers(HttpMethod.POST,"/users/**").authenticated()
+		.antMatchers(HttpMethod.POST).permitAll()
+		.anyRequest().authenticated()
+		.and()
+		.rememberMe().tokenValiditySeconds(86400).key("rememberMe-Key")
+		.and()
+		.csrf().disable()
+		.formLogin().loginPage("/login/page")
+		//登入成功:導到原先瀏覽頁面，沒有就welcome
+		.defaultSuccessUrl("/login/welcome");
+	}
+	
+}
