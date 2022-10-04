da8144153328e683f41e37a5af04b0f850a7eced
Shiululu <tt8otw@gmail.com>
2022-10-04 18:36:46 +0800

    221004 Luana 更新cart,order

--- src/main/java/tw/luana/order/model/HotelOrderRepository.java
+++ src/main/java/tw/luana/order/model/HotelOrderRepository.java
@@ -3,10 +3,10 @@ package tw.luana.order.model;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface OrderRepository extends JpaRepository<Orders, Integer> {
+public interface HotelOrderRepository extends JpaRepository<HotelOrder, Integer> {
 
-	public Orders findByOrderid(Integer orderid);
+	public HotelOrder findByreservationid(Integer reservationid);
 
-
+	
 	
 }
---
+++ src/main/java/tw/luana/order/model/OrderList.java
@@ -0,0 +1,110 @@
+package tw.luana.order.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ORDERLIST")
+public class OrderList implements Serializable {
+
+	@Id
+	@Column(name = "ORDERID")
+	private String orderid;
+	
+	@Column(name = "ORDERTYPE")
+	private Integer ordertype;
+	
+	@Column(name = "ORDERDATE")
+	private String orderdate;
+	
+	@Column(name = "ORDERSTATUS")
+	private String orderstatus;
+	
+	@Column(name = "TOTALPRICE")
+	private Integer totalprice;
+	
+	@Column(name = "MEMBERID")
+	private Integer memberid;
+	
+	
+	
+	public OrderList() {
+	}
+
+
+
+	public String getOrderid() {
+		return orderid;
+	}
+
+
+
+	public void setOrderid(String orderid) {
+		this.orderid = orderid;
+	}
+
+
+
+	public Integer getOrdertype() {
+		return ordertype;
+	}
+
+
+
+	public void setOrdertype(Integer ordertype) {
+		this.ordertype = ordertype;
+	}
+
+
+
+	public String getOrderdate() {
+		return orderdate;
+	}
+
+
+
+	public void setOrderdate(String orderdate) {
+		this.orderdate = orderdate;
+	}
+
+
+
+	public String getOrderstatus() {
+		return orderstatus;
+	}
+
+
+
+	public void setOrderstatus(String orderstatus) {
+		this.orderstatus = orderstatus;
+	}
+
+
+
+	public Integer getTotalprice() {
+		return totalprice;
+	}
+
+
+
+	public void setTotalprice(Integer totalprice) {
+		this.totalprice = totalprice;
+	}
+
+
+
+	public Integer getMemberid() {
+		return memberid;
+	}
+
+
+
+	public void setMemberid(Integer memberid) {
+		this.memberid = memberid;
+	}
+
+}
